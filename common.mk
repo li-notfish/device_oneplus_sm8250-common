441dea36b8e049601fd1990fdc35ae140f007f7a
linotfish <linotfish@Outlook.com>
2024-07-01 21:15:31 +0800

    sm8250-common: Remove Some gapps

--- common.mk
+++ common.mk
@@ -365,6 +365,10 @@ PRODUCT_PACKAGES += \
     libsqlite.vendor:64 \
     libvndfwk_detect_jni.qti.vendor
 
+# Remove Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
