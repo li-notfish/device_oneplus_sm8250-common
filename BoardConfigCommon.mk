4ce3c623afd99bcfa5762307c107c89c8526119e
onlyfish <linotfish@outlook.com>
2025-01-07 21:55:28 +0800

    sm8250-common: remove Taget Clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,7 +115,6 @@ BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=oneplus
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
 TARGET_KERNEL_CONFIG := kona_defconfig vendor/debugfs.config
-TARGET_KERNEL_CLANG_VERSION := r487747c
 
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
